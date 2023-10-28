02daf5ccf0e6abb7d83325bc4b2b21d4269a9fd3
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-10-28 10:50:29 +0300

    create api for projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        return json_encode(['data' => Project::all(['id', 'name', 'created_at'])]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:2|string',
+        ]);
+
+        $project = new Project;
+        $project->name = $request->input('name');
+        $project->save();
+
+        return $project;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(ProjectController::class)->group(function () {
+    Route::get('/projects', 'index');
+    Route::post('/project', 'store');
 });
